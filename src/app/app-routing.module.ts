b3dec36d7c94b996105e405c53cc9467ad09794d
Muhammad Amirrullah muhammad_17005398 <muhammad_17005398@utp.edu.my>
2021-10-11 13:21:09 +0800

    routing dashboard and activity

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
-import { NgModule } from '@angular/core';
+import { ActivityLogComponent } from './activity-log/activity-log.component';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '',
+  component: DashboardComponent
+  },
+  {path: 'activityLog',
+   component: ActivityLogComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
+import { ActivityLogComponent } from './activity-log/activity-log.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ActivityLogComponent,
+    DashboardComponent,
   ],
   imports: [
     BrowserModule,
