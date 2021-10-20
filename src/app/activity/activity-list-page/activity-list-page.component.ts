9f4963805db5079a5be683e5146d5f8ad2ded25c
Muhammad Amirrullah muhammad_17005398 <muhammad_17005398@utp.edu.my>
2021-10-20 19:49:29 +0800

    activity

--- src/app/activity/activity-list-page/activity-list-page.component.ts
+++ src/app/activity/activity-list-page/activity-list-page.component.ts
@@ -1,6 +1,7 @@
 
-import { AfterViewInit, Component, HostListener, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, HostListener, OnInit, Output, ViewChild, Input } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
+import { throwMatDuplicatedDrawerError } from '@angular/material/sidenav';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { Subscription } from 'rxjs';
@@ -21,21 +22,51 @@ export class ActivityListPageComponent implements OnInit  {
 
   constructor( public xtvtdb : ActivitydbService) {}
   
-  ngOnInit(): void {
-    this.innerHeight = window.innerHeight;
-    
-    this.sub = this.xtvtdb.getAcitivtyLog()
-    .subscribe(log => (this.activityBoard = log));
-  }
-
   innerHeight: any;
   @HostListener('window:resize', ['$event'])
   onResize() {
   this.innerHeight = window.innerHeight;
   }
 
+  ngOnInit(): void {
+    this.innerHeight = window.innerHeight;
+    
+    this.sub = this.xtvtdb.getAcitivtyLog()
+    .subscribe(log => (this.activityBoard = log));
+
+    this.isDisplayed = false
+  }
+
   ngOnDestroy() {
     this.sub.unsubscribe();
+  }
+
+  @Input() data: any  ;
+  title:string;
+  isDisplayed : boolean = false;
+  activityDate : any;
+  notes : string;
+
+
+  showLogDetails(value: any){
+    try {
+      this.isDisplayed= true
+      this.data = value;
+      this.title = this.data.title;
+      this.activityDate = Date.parse(this.toDate(this.data.date.toString()));
+      this.notes = this.data.notes;
+    } catch (error) {
+      
+    }
+  }
+
+  toDate(input: string){
+    const year = input.slice(0,4);
+    const month = input.slice(4,6);
+    const day = input.slice(6,8);
+    const date = month + " " + day + " " + year;
+    
+    return date
   }
 
 }
