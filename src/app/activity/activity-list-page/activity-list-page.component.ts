0f318744b3396d4bd3833a9bd1717bcd091fbd78
Muhammad Amirrullah muhammad_17005398 <muhammad_17005398@utp.edu.my>
2021-10-21 09:04:30 +0800

    time and note display

--- src/app/activity/activity-list-page/activity-list-page.component.ts
+++ src/app/activity/activity-list-page/activity-list-page.component.ts
@@ -43,6 +43,8 @@ export class ActivityListPageComponent implements OnInit  {
   isDisplayed : boolean = false;
   activityDate : any;
   notes : string;
+  timeStart: any;
+  timeEnd : any;
 
 
   showLogDetails(value: any){
@@ -52,6 +54,8 @@ export class ActivityListPageComponent implements OnInit  {
       this.title = this.data.title;
       this.activityDate = Date.parse(this.toDate(this.data.date.toString()));
       this.notes = this.data.notes;
+      this.timeStart = Date.parse(this.toDateTime(this.data.time.starttime.toString()));
+      this.timeEnd = Date.parse(this.toDateTime(this.data.time.endtime.toString()));
     } catch (error) {
       
     }
@@ -64,6 +68,17 @@ export class ActivityListPageComponent implements OnInit  {
     const date = month + " " + day + " " + year;
     
     return date
+  }
+
+  toDateTime(input : string){
+    const year = input.slice(4,8);
+    const month = input.slice(2,4);
+    const day = input.slice(0,2);
+    const hour = input.slice(9,11);
+    const minute = input.slice(11,13);
+    const longDate = month + " " + day + " " + year + " " + hour + ":"+ minute;
+    console.log(longDate);
+    return longDate;
   }
 
 }
