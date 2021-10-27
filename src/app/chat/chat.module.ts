554ebb366ce252055e65d13b5874528c1ad8933e
Muhammad Amirrullah muhammad_17005398 <muhammad_17005398@utp.edu.my>
2021-10-28 01:01:36 +0800

    next to populate data from db using

--- src/app/chat/chat.module.ts
+++ src/app/chat/chat.module.ts
@@ -1,3 +1,4 @@
+import { ChatRoutingModule } from './chat-routing.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ChatBoardListComponent } from './chat-board-list/chat-board-list.component';
@@ -18,6 +19,7 @@ import { MatTableModule } from '@angular/material/table';
     SharedModule,
     MatPaginatorModule,
     MatTableModule,
+    ChatRoutingModule
   ]
 })
 export class ChatModule { }
