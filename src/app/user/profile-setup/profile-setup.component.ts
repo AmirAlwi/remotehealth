bcfb87e375738f5548c46b4b90acce5dfaaae466
Muhammad Amirrullah muhammad_17005398 <muhammad_17005398@utp.edu.my>
2021-11-05 15:50:17 +0800

    date is ok now

--- src/app/user/profile-setup/profile-setup.component.ts
+++ src/app/user/profile-setup/profile-setup.component.ts
@@ -1,16 +1,36 @@
 import { Component, OnInit } from '@angular/core';
-import { Validators, FormBuilder, FormGroup } from '@angular/forms';
+import { FormControl, Validators, FormBuilder, FormGroup } from '@angular/forms';
+import {DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE} from '@angular/material/core';
+import {MomentDateAdapter, MAT_MOMENT_DATE_ADAPTER_OPTIONS} from '@angular/material-moment-adapter';
+
+// export const MY_FORMATS = {
+//   parse: {
+//     dateInput: 'DD/MM/YYYY',
+//   },
+//   display: {
+//     dateInput: 'DD/MM/YYYY',
+//     monthYearLabel: 'MM',
+//     dateA11yLabel: 'MM',
+//     monthYearA11yLabel: 'MM YYYY',
+//   },
+// };
+
 @Component({
   selector: 'app-profile-setup',
   templateUrl: './profile-setup.component.html',
-  styleUrls: ['./profile-setup.component.scss']  
+  styleUrls: ['./profile-setup.component.scss'],  
+  // providers: [{
+  //   provide: DateAdapter,
+  //   useClass: MomentDateAdapter,
+  //   deps: [MAT_DATE_LOCALE, MAT_MOMENT_DATE_ADAPTER_OPTIONS]
+  // },{provide: MAT_DATE_FORMATS, useValue: MY_FORMATS}]
 })
-
 export class ProfileSetupComponent implements OnInit {
   form :FormGroup;
   serverMessage: any;
   loading = false;
   constructor(private fb : FormBuilder) { }
+  test : any;
 
   ngOnInit(): void {
     this.form = this.fb.group({
@@ -29,7 +49,8 @@ export class ProfileSetupComponent implements OnInit {
     const gender = this.gender?.value;
     const lname = this.lname?.value;
     const fname = this.fname?.value;
-    const birthdate = this.birthdate?.value;
+    const birthdate = Date.parse(this.birthdate?.value);
+    this.test = birthdate;
     const height = this.height?.value;
     const weight = this.weight?.value;
     try {
@@ -53,7 +74,7 @@ export class ProfileSetupComponent implements OnInit {
     return this.form.get('lname');
   }
 
-  get birthdate(){  
+  get birthdate(){   
     return this.form.get('birthdate');
   }
 
