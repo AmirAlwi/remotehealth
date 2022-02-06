3316b08b622df097cfb9cd6481451c7d5869c34f
Amirrullah Alwi <mayden147@gmail.com>
2022-02-06 11:22:28 +0800

    deploy to firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyDGj1Fouh114V3w65thJrYYNigxP1KjbMQ",
+    authDomain: "isdp-testdb.firebaseapp.com",
+    projectId: "isdp-testdb",
+    storageBucket: "isdp-testdb.appspot.com",
+    messagingSenderId: "978654114965",
+    appId: "1:978654114965:web:410ac406d76f28f3eb979c",
+    measurementId: "G-C9KHG60MNL"
+  }
 };
